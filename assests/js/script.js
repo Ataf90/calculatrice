b687a6c515fd8fbf15997565b124bf5c2589da6f
Ataf_Georges <ataf_eleid@hotmail.com>
2023-10-11 20:01:50 +0200

    commence avec  JS code

--- assests/js/script.js
+++
@@ -1,9 +0,0 @@
-let containerElements = document.createElement("div");
-let elementHeader = document.createElement("h2");
-let elementLabel = document.createElement("label");
-
-for (let numero = 0; numero <=9; numero++) {
-    let elementButton = document.createElement("button");
-    
-}
-
