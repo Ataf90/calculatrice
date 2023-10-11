c419ed161f3da6d8c869d4178841593feedac28b
Ataf_Georges <ataf_eleid@hotmail.com>
2023-10-11 11:36:39 +0200

    crée ma branch

---
+++ assests/js/script.js
@@ -0,0 +1,9 @@
+let containerElements = document.createElement("div");
+let elementHeader = document.createElement("h2");
+let elementLabel = document.createElement("label");
+
+for (let numero = 0; numero <=9; numero++) {
+    let elementButton = document.createElement("button");
+    
+}
+
